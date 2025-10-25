8a5e753a4fadf459462839319f690d87a593dce6
22120061 - Tuan Dat <vttdat28102004@gmail.com>
2025-10-25 21:48:27 +0700

    Initial commit project [Test project unity]

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;   // đối tượng cần theo dõi (player)
+    public float smoothSpeed = 0.125f; // tốc độ mượt
+    public Vector3 offset;     // khoảng cách giữa camera và player
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+    }
+}
+
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // player speed
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Vector2 dominantInput;
+
+    private Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        if (rb == null)
+        {
+            rb = gameObject.AddComponent<Rigidbody2D>();
+            rb.gravityScale = 0; // turnoff gravity
+        }
+
+        dominantInput = new Vector2(0, -1);
+    }
+
+    void Update()
+    {
+        float rawX = Input.GetAxisRaw("Horizontal");
+        float rawY = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(rawX, rawY).normalized;
+
+        if (moveInput.sqrMagnitude > 0.01f)
+        {
+            // W + D -> go D
+            if (Mathf.Abs(rawX) > Mathf.Abs(rawY))
+            {
+                dominantInput = new Vector2(rawX, 0);
+            }
+            // 
+            else
+            {
+                dominantInput = new Vector2(0, rawY);
+            }
+        }
+
+        anim.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        anim.SetFloat("MoveX", dominantInput.x);
+        anim.SetFloat("MoveY", dominantInput.y);
+    }
+
+    void FixedUpdate()
+    { // Move player
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime); }
+    }
+
