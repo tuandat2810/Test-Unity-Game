0f18d495e9efa5bbb13bdee5e65392ef48b0a4ae
22120061 - Tuan Dat <vttdat28102004@gmail.com>
2025-11-06 21:14:25 +0700

    Inventory: Change using items logic from PlayerCombat to InventoryInput.

---
+++ Assets/Scripts/Player/InventoryInput.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+// This script's only job is to listen for inventory hotkeys.
+[RequireComponent(typeof(InventoryManager))]
+public class InventoryInput : MonoBehaviour
+{
+    private InventoryManager inventoryManager;
+
+    void Start()
+    {
+        // Get the InventoryManager component on this same GameObject
+        inventoryManager = GetComponent<InventoryManager>();
+    }
+
+    void Update()
+    {
+        // These inputs should work regardless of combat state,
+        // so we don't need to check the state.
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            inventoryManager.UseItem(0); // Use item in slot 0
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        {
+            inventoryManager.UseItem(1); // Use item in slot 1
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) 
+        {
+            inventoryManager.UseItem(2); // Use item in slot 2
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) 
+        {
+            inventoryManager.UseItem(3); // Use item in slot 3
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) 
+        {
+            inventoryManager.UseItem(4); // Use item in slot 4
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6)) 
+        {
+            inventoryManager.UseItem(5); // Use item in slot 5
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7)) 
+        {
+            inventoryManager.UseItem(6); // Use item in slot 6
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8)) 
+        {
+            inventoryManager.UseItem(7); // Use item in slot 7
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9)) 
+        {
+            inventoryManager.UseItem(8); // Use item in slot 8
+        }
+    }
+}
